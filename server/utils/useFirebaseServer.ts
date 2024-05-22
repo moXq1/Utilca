15be5130cc515df7bb43eb0c48e20423c867f1af
kkkkkk <nikitayanc@gmail.com>
2024-05-22 12:16:13 +0300

    service-account

--- server/utils/useFirebaseServer.ts
+++ server/utils/useFirebaseServer.ts
@@ -3,7 +3,11 @@ import { initializeApp, getApps, getApp, deleteApp } from "firebase/app";
 import { getDatabase, ref, get } from "firebase/database";
 import admin from "firebase-admin";
 
+//@ts-ignore
+const cred = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+
 const config = {
+  credential: admin.credential.cert(cred),
   apiKey: process.env.apiKey,
   authDomain: process.env.authDomain,
   databaseURL: process.env.databaseURL,
